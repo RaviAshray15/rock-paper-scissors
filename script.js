cb7ced097f39687516d728db96231817c3b3469e
Ravi Ashray <141756072+RaviAshray15@users.noreply.github.com>
2025-03-19 17:11:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const containerDisplay = document.getElementById("containerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScore");
+const computerScoreDisplay = document.getElementById("computerScore");
+let playerScore = 0;
+let computerScore = 0;
+
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = '';
+
+    if (playerChoice === computerChoice) {
+        result = "It's a Tie!";
+    }
+    else {
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors') {
+                result = "You win!!!";
+            }
+            else {
+                result = "You lose!";
+            }
+        }
+        else if (playerChoice === 'paper') {
+            if (computerChoice === 'rock') {
+                result = "You win!!!";
+            }
+            else {
+                result = "You lose!";
+            }
+        }
+        else if (playerChoice === 'scissors') {
+            if (computerChoice === 'paper') {
+                result = "You win!!!";
+            }
+            else {
+                result = "You lose!";
+            }
+        }
+    }
+    playerDisplay.textContent = `Player : ${playerChoice}`;
+    computerDisplay.textContent = `Computer : ${computerChoice}`;
+    resultDisplay.textContent = result.toUpperCase();
+
+    
+    if (result == "You win!!!") {
+        playerScore++;
+        resultDisplay.style.color = "lime";
+    }
+    else if (result == "You lose!") {
+        computerScore++;
+        resultDisplay.style.color = "red";
+    }
+    else{
+        resultDisplay.style.color = "#0DCAF0";
+    }
+
+    playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+}
